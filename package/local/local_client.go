4108c4a6229298d7f79d874e5f7eb246a8a66b5f
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-10-11 23:42:58 +0200

    style: outsource SID struct

--- package/local/local_client.go
+++ package/local/local_client.go
@@ -12,3 +12,9 @@ type Client struct {
 func New(conn *connection.Connection) *Client {
 	return &Client{Connection: conn}
 }
+
+// SID is a common struct by all security principals
+// The structure we get from powershell contains more fields, but we're only interested in the Value.
+type SID struct {
+	Value string `json:"Value"`
+}
