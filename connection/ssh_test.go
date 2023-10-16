a1152cad3ea719587bbfa4fb4a7f09ef69bf1c88
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-10-16 17:37:20 +0200

    chore: remove comment

--- connection/ssh_test.go
+++ connection/ssh_test.go
@@ -5,25 +5,6 @@ import (
 )
 
 func TestNewSSHClient(t *testing.T) {
-	// t.Run("ValidConfig", func(t *testing.T) {
-
-	// 	config := &SSHConfig{
-	// 		SSHHost:     os.Getenv("GOWINDOWS_TEST_SSH_HOST"),
-	// 		SSHPort:     22,
-	// 		SSHUsername: os.Getenv("GOWINDOWS_TEST_SSH_USERNAME"),
-	// 		SSHPassword: os.Getenv("GOWINDOWS_TEST_SSH_PASSWORD"),
-	// 	}
-
-	// 	client, err := newSSHClient(config)
-	// 	if err != nil {
-	// 		t.Errorf("Expected no error, but got %v", err)
-	// 	}
-	// 	if client == nil {
-	// 		t.Error("Expected a non-nil SSH client, but got nil")
-	// 	}
-
-	// })
-
 	t.Run("InvalidConfig", func(t *testing.T) {
 		// Test case with missing required fields
 		config := &SSHConfig{}
