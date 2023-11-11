99ff835af117d54bbc48b404c5cfd3cdacd951e2
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-11-11 21:01:08 +0100

    feat: add local-GroupCreate function

--- windows/local/local_group.go
+++ windows/local/local_group.go
@@ -119,3 +119,56 @@ func (c *Client) GroupList(ctx context.Context) (*[]Group, error) {
 
 	return &gs, nil
 }
+
+// GroupCreate creates a new group and returns the Group object
+func (c *Client) GroupCreate(ctx context.Context, params GroupParams) (*Group, error) {
+
+	// Assert needed parameters
+	if params.Name == "" {
+		return nil, errors.New("Name must be set")
+	}
+
+	// Base command
+	cmds := []string{"New-LocalGroup"}
+
+	// Add parameters
+	cmds = append(cmds, fmt.Sprintf("-Name '%s'", params.Name))
+
+	if params.Description != "" {
+		cmds = append(cmds, fmt.Sprintf("-Description '%s'", params.Description))
+	}
+
+	cmd := strings.Join(cmds, " ")
+
+	// Optional parameters
+	opts := &parser.PwshOpts{
+		JSONOutput: true,
+	}
+
+	// Powershell command object
+	pwshCmd, err := parser.NewPwshCommand([]string{cmd}, opts)
+
+	// Run the comand
+	result, err := c.Connection.Run(ctx, pwshCmd)
+	if err != nil {
+		return nil, err
+	}
+
+	// Handle stderr
+	if result.StdErr != "" {
+		errXML, err := parser.DecodeCLIXML(result.StdErr)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, errors.New(errXML)
+	}
+
+	// Unmarshal result
+	err = json.Unmarshal([]byte(result.StdOut), &g)
+	if err != nil {
+		return nil, err
+	}
+
+	return &g, nil
+}
