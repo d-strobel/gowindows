25416b9cc427bc03a4c265ea98e4278ce677de99
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-04-19 17:47:46 +0200

    docs: add comment for connection interface

--- connection/connection.go
+++ connection/connection.go
@@ -8,7 +8,12 @@ import (
 // Connection defines the interface for a connection.
 // Every connection type must implement this interface.
 type Connection interface {
+
+	// Run runs a command using the configured connection and context.
+	// It returns the result of the command execution.
 	Run(ctx context.Context, cmd string) (CMDResult, error)
+
+	// Close closes any open connection.
 	Close() error
 }
 
