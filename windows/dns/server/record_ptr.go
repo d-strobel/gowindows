8f558162aabc52e7e230f627716d30a0b2993c61
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-12-08 15:52:34 +0100

    feat: add dnsserver PTR-Record functions

---
+++ windows/dns/server/record_ptr.go
@@ -0,0 +1,246 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/d-strobel/gowindows/winerror"
+)
+
+// RecordPTR represents a DNS PTR-Record.
+type RecordPTR struct {
+	DistinguishedName string
+	Name              string
+	PTR               string
+	Timestamp         time.Time
+	TimeToLive        int32
+}
+
+// convertOutput converts the unmarshaled JSON output from the recordObject to a RecordPTR object.
+func (r *RecordPTR) convertOutput(o recordObject) {
+	r.DistinguishedName = o.DistinguishedName
+	r.Name = o.Name
+	r.Timestamp = o.Timestamp.Time
+	r.TimeToLive = o.TimeToLive.Seconds
+	r.PTR = o.RecordData.CimInstanceProperties["PtrDomainName"]
+}
+
+// RecordPTRReadParams represents parameters for the PTR-Record function.
+type RecordPTRReadParams struct {
+	// Specifies the name of the Record.
+	Name string
+
+	// Specifies the zone in which the record is located.
+	Zone string
+}
+
+// pwshCommand returns the PowerShell command to read a DNS PTR-Record.
+func (params RecordPTRReadParams) pwshCommand() string {
+	// Base command
+	cmd := []string{"Get-DnsServerResourceRecord -RRType 'PTR' -Node"}
+
+	// Add parameters
+	cmd = append(cmd, fmt.Sprintf("-Name '%s'", params.Name))
+	cmd = append(cmd, fmt.Sprintf("-ZoneName '%s'", params.Zone))
+
+	// Ensure Json Output
+	cmd = append(cmd, "| ConvertTo-Json -Compress")
+	return strings.Join(cmd, " ")
+}
+
+// RecordPTRRead gets a DNS PTR-Record by Name and Zone. It returns a RecordPTR object.
+// It returns a *winerror.WinError if the windows client returns an error.
+func (c *Client) RecordPTRRead(ctx context.Context, params RecordPTRReadParams) (RecordPTR, error) {
+	var r RecordPTR
+	var o recordObject
+
+	// Assert needed parameters
+	if params.Name == "" || params.Zone == "" {
+		return r, errors.New("windows.dns.server.RecordPTRRead: record parameters 'Name' and 'Zone' must be set")
+	}
+
+	// Run command
+	cmd := params.pwshCommand()
+	if err := run(ctx, c, cmd, &o); err != nil {
+		return r, winerror.Errorf(cmd, "windows.dns.server.RecordPTRRead: %s", err)
+	}
+
+	// Convert the output to a RecordPTR object.
+	r.convertOutput(o)
+
+	return r, nil
+}
+
+// RecordPTRCreateParams represents parameters for the PTR-Record function.
+type RecordPTRCreateParams struct {
+	// Specifies the name of the Record.
+	Name string
+
+	// Specifies the zone in which the record is located.
+	Zone string
+
+	// Specifies the PTR of the record.
+	PTR string
+
+	// Specifies the time to live (TTL) of the record in seconds.
+	// If not provided, the default is 86400 seconds.
+	// A TTL of 0 is not allowed.
+	TimeToLive int32
+}
+
+// pwshCommand returns the PowerShell command to create a new PTR-Record.
+func (params RecordPTRCreateParams) pwshCommand() string {
+	// Base command
+	cmd := []string{"Add-DnsServerResourceRecordPTR -AllowUpdateAny:$false -AgeRecord:$false -Confirm:$false -PassThru"}
+
+	// Add parameters
+	cmd = append(cmd, fmt.Sprintf("-Name '%s'", params.Name))
+	cmd = append(cmd, fmt.Sprintf("-ZoneName '%s'", params.Zone))
+	cmd = append(cmd, fmt.Sprintf("-PtrDomainName '%s'", params.PTR))
+
+	// Set default TTL if not provided.
+	// New-TimeSpan only allows int32 values.
+	// https://learn.microsoft.com/de-de/powershell/module/microsoft.powershell.utility/new-timespan?view=powershell-7.4
+	if params.TimeToLive == 0 {
+		params.TimeToLive = defaultTimeToLive
+	}
+	cmd = append(cmd, fmt.Sprintf("-TimeToLive %s", fmt.Sprintf("$(New-TimeSpan -Seconds %d)", params.TimeToLive)))
+
+	// Join the command and ensure Json Output
+	cmd = append(cmd, "| ConvertTo-Json -Compress")
+
+	return strings.Join(cmd, " ")
+}
+
+// RecordPTRCreate creates a DNS PTR-Record. It returns a RecordPTR object.
+// It returns a *winerror.WinError if the windows client returns an error.
+func (c *Client) RecordPTRCreate(ctx context.Context, params RecordPTRCreateParams) (RecordPTR, error) {
+	var r RecordPTR
+	var o recordObject
+
+	// Assert needed parameters
+	if params.Name == "" || params.Zone == "" || params.PTR == "" {
+		return r, errors.New("windows.dns.server.RecordPTRCreate: record parameters 'Name', 'Zone' and 'PTR' must be set")
+	}
+
+	// Run command
+	cmd := params.pwshCommand()
+	if err := run(ctx, c, cmd, &o); err != nil {
+		// Handle record already exists error.
+		if strings.Contains(err.Error(), "ResourceExists") {
+			return r, winerror.Errorf(cmd, "windows.dns.server.RecordPTRCreate: the specified record already exists.")
+		}
+
+		return r, winerror.Errorf(cmd, "windows.dns.server.RecordPTRCreate: %s", err)
+	}
+
+	// Convert the output to a RecordPTR object.
+	r.convertOutput(o)
+
+	return r, nil
+}
+
+// RecordPTRUpdateParams represents parameters for the PTR-Record function.
+// The PTR and TimeToLive can be updated.
+type RecordPTRUpdateParams struct {
+	// Specifies the name of the Record.
+	Name string
+
+	// Specifies the zone in which the record is located.
+	Zone string
+
+	// Specifies the PTR of the record.
+	PTR string
+
+	// Specifies the time to live (TTL) of the record in seconds.
+	// If not provided, the default TTL is 86400 seconds.
+	// A TTL of 0 is not allowed.
+	TimeToLive int32
+}
+
+// pwshCommand returns the PowerShell command to create a new PTR-Record.
+func (params RecordPTRUpdateParams) pwshCommand() string {
+	// Update to default TTL if not provided.
+	// New-TimeSpan only allows int32 values.
+	// https://learn.microsoft.com/de-de/powershell/module/microsoft.powershell.utility/new-timespan?view=powershell-7.4
+	if params.TimeToLive == 0 {
+		params.TimeToLive = defaultTimeToLive
+	}
+
+	// Get command
+	cmd := []string{"$r=Get-DnsServerResourceRecord -RRType 'PTR' -Node"}
+	cmd = append(cmd, fmt.Sprintf("-Name '%s'", params.Name))
+	cmd = append(cmd, fmt.Sprintf("-ZoneName '%s'", params.Zone))
+
+	// Add logic for handling TTL and PTR update.
+	cmd = append(cmd, ";$n=[ciminstance]::new($r)")
+	cmd = append(cmd, fmt.Sprintf(";$n.TimeToLive=New-TimeSpan -Seconds %d", params.TimeToLive))
+	cmd = append(cmd, fmt.Sprintf(";$n.RecordData.PtrDomainName='%s'", params.PTR))
+	cmd = append(cmd, fmt.Sprintf(";Set-DnsServerResourceRecord -OldInputObject $r -NewInputObject $n -ZoneName '%s' -PassThru", params.Zone))
+
+	// Ensure Json Output
+	cmd = append(cmd, "| ConvertTo-Json -Compress")
+
+	// Return the full command.
+	return strings.Join(cmd, " ")
+}
+
+// RecordPTRUpdate updates a DNS PTR-Record. It returns a RecordPTR object.
+// It returns a *winerror.WinError if the windows client returns an error.
+func (c *Client) RecordPTRUpdate(ctx context.Context, params RecordPTRUpdateParams) (RecordPTR, error) {
+	var r RecordPTR
+	var o recordObject
+
+	// Assert needed parameters
+	if params.Name == "" || params.Zone == "" || params.PTR == "" {
+		return r, errors.New("windows.dns.server.RecordPTRUpdate: record parameters 'Name', 'Zone' and 'PTR' must be set")
+	}
+
+	// Run command
+	cmd := params.pwshCommand()
+	if err := run(ctx, c, cmd, &o); err != nil {
+		return r, winerror.Errorf(cmd, "windows.dns.server.RecordPTRUpdate: %s", err)
+	}
+
+	// Convert the output to a RecordPTR object.
+	r.convertOutput(o)
+
+	return r, nil
+}
+
+// RecordPTRDeleteParams represents parameters for the PTR-Record delete function.
+type RecordPTRDeleteParams struct {
+	// Specifies the name of the Record.
+	Name string
+
+	// Specifies the zone in which the record is located.
+	Zone string
+}
+
+// pwshCommand returns the PowerShell command to delete a PTR-Record.
+func (params RecordPTRDeleteParams) pwshCommand() string {
+	// Base command
+	return fmt.Sprintf("Remove-DnsServerResourceRecord -RRType 'PTR' -Force -Name '%s' -ZoneName '%s'", params.Name, params.Zone)
+}
+
+// RecordPTRDelete deletes a DNS PTR-Record.
+// It returns a *winerror.WinError if the windows client returns an error.
+func (c *Client) RecordPTRDelete(ctx context.Context, params RecordPTRDeleteParams) error {
+	var o recordObject
+
+	// Assert needed parameters
+	if params.Name == "" || params.Zone == "" {
+		return errors.New("windows.dns.server.RecordPTRDelete: record parameters 'Name' and 'Zone' must be set")
+	}
+
+	// Run command
+	cmd := params.pwshCommand()
+	if err := run(ctx, c, cmd, &o); err != nil {
+		return winerror.Errorf(cmd, "windows.dns.server.RecordPTRDelete: %s", err)
+	}
+
+	return nil
+}
---
+++ windows/dns/server/record_ptr_test.go
@@ -0,0 +1,257 @@
+package server
+
+import (
+	"context"
+	"time"
+
+	"github.com/d-strobel/gowindows/connection"
+	mockConnection "github.com/d-strobel/gowindows/connection/mocks"
+	"github.com/d-strobel/gowindows/parsing"
+)
+
+// Fixtures
+const (
+	recordPTRJson = `{"DistinguishedName":"DC=1,DC=10.168.192.in-addr.arpa,cn=MicrosoftDNS,DC=DomainDnsZones,DC=test,DC=local","Hostname":"1","RecordType":"PTR","Timestamp":null,"timetolive":{"Ticks":36000000000,"Days":0,"Hours":1,"Milliseconds":0,"Minutes":0,"Seconds":0,"TotalDays":0.041666666666666664,"TotalHours":1,"TotalMilliseconds":3600000,"TotalMinutes":60,"TotalSeconds":3600},"RecordData":{"CimClass":"root/Microsoft/Windows/DNS:DnsServerResourceRecordPTR","CimInstanceProperties":["PtrDomainName = \"testptr.test.local.\""],"CimSystemProperties":"Microsoft.Management.Infrastructure.CimSystemProperties"},"Type":1}`
+
+	recordPTRExistsErr = `Fehler beim Erstellen des Ressourcendatensatzes "terratest" in der Zone "test.local" auf dem Server "DC-01".In Zeile:1 Zeichen:43
+        ... yContinue'; Add-DnsServerResourceRecordPTR -AllowUpdateAny:$false -Crea ...
+                        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        CategoryInfo          : ResourceExists: (terratest:root/Microsoft/...ResourceRecordPTR) [Add-DnsServerResourceReco rdA], CimException
+        FullyQualifiedErrorId : WIN32 9711,Add-DnsServerResourceRecordPTR)
+	`
+)
+
+var (
+	expectedRecordPTR = RecordPTR{
+		DistinguishedName: "DC=1,DC=10.168.192.in-addr.arpa,cn=MicrosoftDNS,DC=DomainDnsZones,DC=test,DC=local",
+		Name:              "1",
+		Timestamp:         time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+		TimeToLive:        3600,
+		PTR:               "testptr.test.local.",
+	}
+)
+
+// Test the convertOutput method.
+func (suite *DnsServerUnitTestSuite) TestRecordPTRConvertOutput() {
+	suite.Run("should return the correct object", func() {
+		tcs := []struct {
+			description          string
+			expectedRecordPTR    RecordPTR
+			inputRecordPTRObject recordObject
+		}{
+			{
+				"should return the correct RecordPTR object",
+				RecordPTR{
+					Name:              "1",
+					DistinguishedName: "DC=1,DC=10.168.192.in-addr.arpa,cn=MicrosoftDNS,DC=DomainDnsZones,DC=test,DC=local",
+					Timestamp:         time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+					TimeToLive:        3600,
+					PTR:               "testptr.test.local.",
+				},
+				recordObject{
+					DistinguishedName: "DC=1,DC=10.168.192.in-addr.arpa,cn=MicrosoftDNS,DC=DomainDnsZones,DC=test,DC=local",
+					Name:              "1",
+					RecordType:        "PTR",
+					Timestamp:         parsing.DotnetTime{},
+					TimeToLive:        timeToLive{Seconds: 3600},
+					RecordData: recordRecordData{
+						CimInstanceProperties: parsing.CimClassKeyVal{
+							"PtrDomainName": "testptr.test.local.",
+						},
+					},
+				},
+			},
+		}
+
+		for _, tc := range tcs {
+			suite.T().Logf("test case: %s", tc.description)
+			r := RecordPTR{}
+			r.convertOutput(tc.inputRecordPTRObject)
+			suite.Equal(tc.expectedRecordPTR, r)
+		}
+	})
+}
+
+// Test RecordPTRRead related methods.
+func (suite *DnsServerUnitTestSuite) TestRecordPTRReadPwshCommand() {
+	suite.Run("should return the correct command", func() {
+		tcs := []struct {
+			description     string
+			inputParameters RecordPTRReadParams
+			expectedCmd     string
+		}{
+			{
+				"assert correct command by name and zone",
+				RecordPTRReadParams{Name: "1", Zone: "10.168.192.in-addr.arpa"},
+				"Get-DnsServerResourceRecord -RRType 'PTR' -Node -Name '1' -ZoneName '10.168.192.in-addr.arpa' | ConvertTo-Json -Compress",
+			},
+		}
+
+		for _, tc := range tcs {
+			suite.T().Logf("test case: %s", tc.description)
+			actualCmd := tc.inputParameters.pwshCommand()
+			suite.Equal(tc.expectedCmd, actualCmd)
+		}
+	})
+}
+
+func (suite *DnsServerUnitTestSuite) TestRecordPTRRead() {
+	suite.Run("should return the correct PTR-Record", func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		mockConn := mockConnection.NewMockConnection(suite.T())
+		c := &Client{
+			Connection:      mockConn,
+			decodeCliXmlErr: func(s string) (string, error) { return "", nil },
+		}
+		mockConn.EXPECT().
+			RunWithPowershell(ctx, "Get-DnsServerResourceRecord -RRType 'PTR' -Node -Name '1' -ZoneName '10.168.192.in-addr.arpa' | ConvertTo-Json -Compress").
+			Return(connection.CmdResult{StdOut: recordPTRJson}, nil)
+		actualRecordPTR, err := c.RecordPTRRead(ctx, RecordPTRReadParams{Name: "1", Zone: "10.168.192.in-addr.arpa"})
+		suite.NoError(err)
+		suite.Equal(expectedRecordPTR, actualRecordPTR)
+	})
+}
+
+// Test RecordPTRCreate related methods.
+func (suite *DnsServerUnitTestSuite) TestRecordPTRCreatePwshCommand() {
+	suite.Run("should return the correct command", func() {
+		tcs := []struct {
+			description     string
+			inputParameters RecordPTRCreateParams
+			expectedCmd     string
+		}{
+			{
+				"assert with default ttl parameter",
+				RecordPTRCreateParams{Name: "1", Zone: "10.168.192.in-addr.arpa", PTR: "testptr.test.local."},
+				"Add-DnsServerResourceRecordPTR -AllowUpdateAny:$false -AgeRecord:$false -Confirm:$false -PassThru -Name '1' -ZoneName '10.168.192.in-addr.arpa' -PtrDomainName 'testptr.test.local.' -TimeToLive $(New-TimeSpan -Seconds 86400) | ConvertTo-Json -Compress",
+			},
+			{
+				"assert with ttl parameter",
+				RecordPTRCreateParams{Name: "1", Zone: "10.168.192.in-addr.arpa", TimeToLive: 3600, PTR: "testptr.test.local."},
+				"Add-DnsServerResourceRecordPTR -AllowUpdateAny:$false -AgeRecord:$false -Confirm:$false -PassThru -Name '1' -ZoneName '10.168.192.in-addr.arpa' -PtrDomainName 'testptr.test.local.' -TimeToLive $(New-TimeSpan -Seconds 3600) | ConvertTo-Json -Compress",
+			},
+		}
+
+		for _, tc := range tcs {
+			suite.T().Logf("test case: %s", tc.description)
+			actualCmd := tc.inputParameters.pwshCommand()
+			suite.Equal(tc.expectedCmd, actualCmd)
+		}
+	})
+}
+
+func (suite *DnsServerUnitTestSuite) TestRecordPTRCreate() {
+	suite.Run("should return the correct record", func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		mockConn := mockConnection.NewMockConnection(suite.T())
+		c := &Client{
+			Connection:      mockConn,
+			decodeCliXmlErr: func(s string) (string, error) { return s, nil },
+		}
+		mockConn.EXPECT().
+			RunWithPowershell(ctx, "Add-DnsServerResourceRecordPTR -AllowUpdateAny:$false -AgeRecord:$false -Confirm:$false -PassThru -Name '1' -ZoneName '10.168.192.in-addr.arpa' -PtrDomainName 'testptr.test.local.' -TimeToLive $(New-TimeSpan -Seconds 3600) | ConvertTo-Json -Compress").
+			Return(connection.CmdResult{StdOut: recordPTRJson}, nil)
+		actualRecord, err := c.RecordPTRCreate(ctx, RecordPTRCreateParams{Name: "1", Zone: "10.168.192.in-addr.arpa", PTR: "testptr.test.local.", TimeToLive: 3600})
+		suite.NoError(err)
+		suite.Equal(expectedRecordPTR, actualRecord)
+	})
+
+	suite.Run("should return 'record already exists' error", func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		mockConn := mockConnection.NewMockConnection(suite.T())
+		c := &Client{
+			Connection:      mockConn,
+			decodeCliXmlErr: func(s string) (string, error) { return s, nil },
+		}
+		mockConn.EXPECT().
+			RunWithPowershell(ctx, "Add-DnsServerResourceRecordPTR -AllowUpdateAny:$false -AgeRecord:$false -Confirm:$false -PassThru -Name '1' -ZoneName '10.168.192.in-addr.arpa' -PtrDomainName 'testptr.test.local.' -TimeToLive $(New-TimeSpan -Seconds 3600) | ConvertTo-Json -Compress").
+			Return(connection.CmdResult{StdErr: recordExistsErr}, nil)
+
+		_, err := c.RecordPTRCreate(ctx, RecordPTRCreateParams{Name: "1", Zone: "10.168.192.in-addr.arpa", PTR: "testptr.test.local.", TimeToLive: 3600})
+		suite.EqualError(err, "windows.dns.server.RecordPTRCreate: the specified record already exists.")
+	})
+}
+
+// Test RecordPTRUpdate related methods.
+func (suite *DnsServerUnitTestSuite) TestRecordPTRUpdatePwshCommand() {
+	suite.Run("should return the correct command", func() {
+		tcs := []struct {
+			description     string
+			inputParameters RecordPTRUpdateParams
+			expectedCmd     string
+		}{
+			{
+				"assert with ttl parameter",
+				RecordPTRUpdateParams{Name: "1", Zone: "10.168.192.in-addr.arpa", TimeToLive: 3600, PTR: "testptr.test.local."},
+				"$r=Get-DnsServerResourceRecord -RRType 'PTR' -Node -Name '1' -ZoneName '10.168.192.in-addr.arpa' ;$n=[ciminstance]::new($r) ;$n.TimeToLive=New-TimeSpan -Seconds 3600 ;$n.RecordData.PtrDomainName='testptr.test.local.' ;Set-DnsServerResourceRecord -OldInputObject $r -NewInputObject $n -ZoneName '10.168.192.in-addr.arpa' -PassThru | ConvertTo-Json -Compress",
+			},
+		}
+
+		for _, tc := range tcs {
+			suite.T().Logf("test case: %s", tc.description)
+			actualCmd := tc.inputParameters.pwshCommand()
+			suite.Equal(tc.expectedCmd, actualCmd)
+		}
+	})
+}
+
+func (suite *DnsServerUnitTestSuite) TestRecordPTRUpdate() {
+	suite.Run("should return the correct updated record", func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		mockConn := mockConnection.NewMockConnection(suite.T())
+		c := &Client{
+			Connection:      mockConn,
+			decodeCliXmlErr: func(s string) (string, error) { return s, nil },
+		}
+		mockConn.EXPECT().
+			RunWithPowershell(ctx, "$r=Get-DnsServerResourceRecord -RRType 'PTR' -Node -Name '1' -ZoneName '10.168.192.in-addr.arpa' ;$n=[ciminstance]::new($r) ;$n.TimeToLive=New-TimeSpan -Seconds 3600 ;$n.RecordData.PtrDomainName='testptr.test.local.' ;Set-DnsServerResourceRecord -OldInputObject $r -NewInputObject $n -ZoneName '10.168.192.in-addr.arpa' -PassThru | ConvertTo-Json -Compress").
+			Return(connection.CmdResult{StdOut: recordPTRJson}, nil)
+		actualRecord, err := c.RecordPTRUpdate(ctx, RecordPTRUpdateParams{Name: "1", Zone: "10.168.192.in-addr.arpa", TimeToLive: 3600, PTR: "testptr.test.local."})
+		suite.NoError(err)
+		suite.Equal(expectedRecordPTR, actualRecord)
+	})
+}
+
+// Test RecordPTRDelete related methods.
+func (suite *DnsServerUnitTestSuite) TestRecordPTRDeletePwshCommand() {
+	suite.Run("should return the correct command", func() {
+		tcs := []struct {
+			description     string
+			inputParameters RecordPTRDeleteParams
+			expectedCmd     string
+		}{
+			{
+				"assert with name and zone",
+				RecordPTRDeleteParams{Name: "1", Zone: "10.168.192.in-addr.arpa"},
+				"Remove-DnsServerResourceRecord -RRType 'PTR' -Force -Name '1' -ZoneName '10.168.192.in-addr.arpa'",
+			},
+		}
+
+		for _, tc := range tcs {
+			suite.T().Logf("test case: %s", tc.description)
+			actualCmd := tc.inputParameters.pwshCommand()
+			suite.Equal(tc.expectedCmd, actualCmd)
+		}
+	})
+}
+
+func (suite *DnsServerUnitTestSuite) TestRecordPTRDelete() {
+	suite.Run("should return no error after deletion", func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		mockConn := mockConnection.NewMockConnection(suite.T())
+		c := &Client{
+			Connection:      mockConn,
+			decodeCliXmlErr: func(s string) (string, error) { return s, nil },
+		}
+		mockConn.EXPECT().
+			RunWithPowershell(ctx, "Remove-DnsServerResourceRecord -RRType 'PTR' -Force -Name '1' -ZoneName '10.168.192.in-addr.arpa'").
+			Return(connection.CmdResult{}, nil)
+		err := c.RecordPTRDelete(ctx, RecordPTRDeleteParams{Name: "1", Zone: "10.168.192.in-addr.arpa"})
+		suite.NoError(err)
+	})
+}
