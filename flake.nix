1b0162e7376933ca11b3619057648fa22bb3986f
d-strobel <github.6f1aa@slmails.com>
2025-03-22 00:29:13 +0100

    chore(nix): add pre-commit

--- flake.nix
+++ flake.nix
@@ -3,12 +3,14 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
+    ...
+  } @ inputs: let
     allSystems = [
       "x86_64-linux"
       "aarch64-linux"
@@ -22,12 +24,37 @@
           pkgs = import nixpkgs {inherit system;};
         });
   in {
+    checks = forAllSystems (system: {
+      pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          nixpkgs-fmt.enable = true;
+        };
+      };
+    });
     devShells = forAllSystems ({pkgs}: {
       default = pkgs.mkShell {
         packages = with pkgs; [
           go
           gotools
+          golangci-lint
+          pre-commit
         ];
+
+        shellHook = let
+          pre-commit = "${pkgs.pre-commit}/bin/pre-commit";
+        in
+          /*
+          bash
+          */
+          ''
+            if [ ! -e .git/hooks/pre-commit ]; then
+              echo "Setting up pre-commit hooks..."
+               ${pre-commit} install
+            else
+              echo "Pre-commit hooks already installed."
+            fi
+          '';
       };
     });
   };
